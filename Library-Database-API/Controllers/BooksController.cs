1aa7490951635e74dc8ab21e20042a543933c6cc
Mangesh Mangesh <kanawade84@gmail.com>
2023-01-10 20:41:40 +0530

    Added New API method

--- Library-Database-API/Controllers/BooksController.cs
+++ Library-Database-API/Controllers/BooksController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace Library_Database_API.Controllers {
     public class BooksController : ApiController {
-        
+
         [HttpGet]
         public HttpResponseMessage BooksCategoriesAndPublicationsList() {
             HttpResponseMessage result = null;
@@ -27,16 +27,16 @@ namespace Library_Database_API.Controllers {
         [HttpPost]
         public HttpResponseMessage BooksList(BooksViewModel booksViewModel) {
             HttpResponseMessage result = null;
-            
+
             try {
                 // For Getting Data From Database according to User need
 
                 Books books = new Books();
                 books.BookName = booksViewModel.BookName;
-                books.BookCategoryId= booksViewModel.BookCategoryId;
-                books.BookPublisherId= booksViewModel.BookPublisherId;
-                books.PageSize= booksViewModel.PageSize;
-                books.PageNumber= booksViewModel.PageNumber;    
+                books.BookCategoryId = booksViewModel.BookCategoryId;
+                books.BookPublisherId = booksViewModel.BookPublisherId;
+                books.PageSize = booksViewModel.PageSize;
+                books.PageNumber = booksViewModel.PageNumber;
 
                 // For Sending Data
 
@@ -62,13 +62,56 @@ namespace Library_Database_API.Controllers {
                 // For Getting Data From Database according to User need
 
                 Books books = new Books();
+                books.BookId = booksViewModel.BookId;
                 books.BookName = booksViewModel.BookName;
                 books.BookCategoryId = booksViewModel.BookCategoryId;
                 books.BookPublisherId = booksViewModel.BookPublisherId;
                 books.BookQuantity = booksViewModel.BookQuantity;
+                books.IsActive = booksViewModel.IsActive;
                 books.Save();
-                
+
                 result = Request.CreateResponse(HttpStatusCode.Created, true);
+
+            } catch (Exception ex) {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+        [HttpPost]
+        public HttpResponseMessage GetBookDetails(BooksViewModel booksViewModel) {
+            HttpResponseMessage result = null;
+
+            try {
+                // For Getting Data From Database according to User need
+
+                Books books = new Books();
+                if (booksViewModel.BookId > 0) {
+                    books.BookId = booksViewModel.BookId;
+                    books.Load();
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, books);
+
+            } catch (Exception ex) {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage DeleteBook(BooksViewModel booksViewModel) {
+            HttpResponseMessage result = null;
+
+            try {
+                // For Getting Data From Database according to User need
+
+                Books books = new Books();
+                if (booksViewModel.BookId > 0) {
+                    books.BookId = booksViewModel.BookId;
+                    books.Load();
+                    books.Delete();
+                    
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, books);
 
             } catch (Exception ex) {
                 result = Request.CreateResponse(HttpStatusCode.BadRequest);
